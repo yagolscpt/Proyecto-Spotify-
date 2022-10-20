53055434a024ecd35aa27cc0abce9ebcb56db3f6
Yago Lopez <90837398+yagolscpt@users.noreply.github.com>
2022-10-20 09:08:11 +0200

    Add files via upload

---
+++ fichero2/Module1.vb
@@ -0,0 +1,16 @@
+﻿Option Strict On
+Imports System.IO
+Module Module1
+    Dim ruta As String = Directory.GetCurrentDirectory() + "\usuarios.txt"
+    Sub Escribir(ByRef ruta As String, ByRef registro As String)
+
+        Dim escritor As StreamWriter = My.Computer.FileSystem.OpenTextFileWriter(ruta, True)
+        escritor.WriteLine(registro)
+        escritor.Close()
+    End Sub
+    Sub Main()
+
+
+    End Sub
+
+End Module
